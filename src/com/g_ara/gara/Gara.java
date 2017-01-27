89c1ab52518603985c2d706786037cf50e0087ec
Ahmed Kamel Taha <ahmedengu@gmail.com>
2017-01-27 18:31:24 +0200

    suppress errors for the defense

--- src/com/g_ara/gara/Gara.java
+++ src/com/g_ara/gara/Gara.java
@@ -2,6 +2,7 @@ package com.g_ara.gara;
 
 
 import com.codename1.analytics.AnalyticsService;
+import com.codename1.io.Log;
 import com.codename1.io.NetworkManager;
 import com.codename1.io.Preferences;
 import com.codename1.notifications.LocalNotificationCallback;
@@ -9,6 +10,8 @@ import com.codename1.ui.Dialog;
 import com.codename1.ui.Display;
 import com.codename1.ui.Form;
 import com.codename1.ui.Toolbar;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.g_ara.gara.controller.UserController;
 import com.parse4cn1.ParseException;
 import userclasses.StateMachine;
@@ -43,16 +46,16 @@ public class Gara implements LocalNotificationCallback {
         }
         new StateMachine("/theme");
 
-//        Display.getInstance().addEdtErrorHandler(new ActionListener() {
-//            public void actionPerformed(ActionEvent evt) {
-//                evt.consume();
-//                Log.p("Exception in Gara version " + Display.getInstance().getProperty("AppVersion", "Unknown"));
-//                Log.p("OS " + Display.getInstance().getPlatformName());
-//                Log.p("Error " + evt.getSource());
-//                Log.p("Current Form " + Display.getInstance().getCurrent().getName());
-//                Log.e((Throwable) evt.getSource());
-//            }
-//        });
+        Display.getInstance().addEdtErrorHandler(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                evt.consume();
+                Log.p("Exception in Gara version " + Display.getInstance().getProperty("AppVersion", "Unknown"));
+                Log.p("OS " + Display.getInstance().getPlatformName());
+                Log.p("Error " + evt.getSource());
+                Log.p("Current Form " + Display.getInstance().getCurrent().getName());
+                Log.e((Throwable) evt.getSource());
+            }
+        });
     }
 
     public void stop() {
